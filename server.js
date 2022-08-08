8494f6a490e63f9780f4f3d0bbc18d5b2be04666
A <andcooke@duck.com>
2022-08-08 17:29:02 -0500

    added jawsdb_url

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  // VVV whatever i want VVV
+  // VVV whatever I want VVV
   secret: 'Super secret secret',
   cookie: {},
   resave: false,
