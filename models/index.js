e58fd38abeac8e928b8fb97a5aa803c4b07319cb
A <andcooke@gmail.com>
2022-08-11 19:52:09 -0500

    models built

--- models/index.js
+++ models/index.js
@@ -7,4 +7,19 @@ Comment.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+})
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+})
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+})
+
+
 module.exports = { Comment, User, Post };
