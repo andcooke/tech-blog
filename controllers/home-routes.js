d9e4c33b4721fed0eed2120709c22be8efc33655
A <andcooke@gmail.com>
2022-08-11 20:26:19 -0500

    home-routes look good, but haven't been tested

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,1 +1,65 @@
- 
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('all-posts', { 
+      posts, 
+      // logged_in: req.session.logged_in *TO-DO*
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+          model: Comment,
+          attributes: ['body', 'date_created', 'user_id'] /* TO-DO */ 
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('single-post', {
+      ...post, /* TO-DO */
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
