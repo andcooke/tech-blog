469bbdfa3bf106d718a5ec33b485f97ee7263290
A <andcooke@gmail.com>
2022-08-21 13:25:37 -0500

    edit posts working

--- public/js/editDelete.js
+++ public/js/editDelete.js
@@ -10,7 +10,24 @@ const deletePostHandler = async (event) => {
   document.location.replace('/dashboard');
 }
 
+const updatePostHandler = async (event) => {
+  event.preventDefault();
+  const post_id = document.querySelector('input[name="post-id"]').value;
+  const title = document.querySelector('input[name="title-input"]').value.trim();
+  const body = document.querySelector('input[name="content-input"]').value.trim(); 
+  await fetch(`/api/post/${post_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ title, body }),
+    headers: { 'Content-Type': 'application/json' },
+  })
+  document.location.replace('/dashboard');
+}
+
+document
+  .querySelector('#update-post-button')
+  .addEventListener('click', updatePostHandler);
 
 document
   .querySelector('#delete-post-button')
   .addEventListener('click', deletePostHandler);
+
